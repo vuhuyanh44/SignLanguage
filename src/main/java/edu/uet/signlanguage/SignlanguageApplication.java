e64443f78fd2dc9499047ff77ada1c3c4fd59c68
huyanh <vuhuyanh44@gmail.com>
2023-03-03 11:06:04 +0700

    first commit

---
+++ src/main/java/edu/uet/signlanguage/SignlanguageApplication.java
@@ -0,0 +1,13 @@
+package edu.uet.signlanguage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignlanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignlanguageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uet/signlanguage/SignlanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uet.signlanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignlanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
